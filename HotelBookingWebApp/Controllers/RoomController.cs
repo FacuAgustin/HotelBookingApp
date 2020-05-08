96932e33ca1db38f8e46af6f010ce89f53948461
Facundo Ojeda <facundoagustinojeda@gmail.com>
2020-05-08 16:35:10 -0300

    Agregar archivos de proyecto.

---
+++ HotelBookingWebApp/Controllers/RoomController.cs
@@ -0,0 +1,127 @@
+﻿using HotelBookingWebApp.Models;
+using HotelBookingWebApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HotelBookingWebApp.Controllers
+{
+    public class RoomController : Controller
+    {
+        HotelBookingDBEntities db = new HotelBookingDBEntities();
+        RoomViewModel objRoomViewModel = new RoomViewModel();
+        public RoomController()
+        {
+
+        }
+        public ActionResult Index()
+        {
+            objRoomViewModel.ListOfBookingStatus = (from d in db.BookingStatus
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text=d.BookingStatus,
+                                                        Value=d.BookingStatusId.ToString()
+                                                    }).ToList();
+            objRoomViewModel.ListOfRoomType= (from d in db.RoomTypes
+                                              select new SelectListItem()
+                                              {
+                                                  Text = d.RoomTypeName,
+                                                  Value = d.RoomTypeId.ToString(),
+                                              }).ToList();
+            return View(objRoomViewModel);
+        }
+        [HttpPost]
+        public JsonResult Index(RoomViewModel objRoomViewModel)
+                {
+            string message = String.Empty;
+            string ImageUniqueName = String.Empty;
+            string ActualImageRoom =String.Empty;
+            if (objRoomViewModel.RoomId==0)
+            {
+                ImageUniqueName = Guid.NewGuid().ToString();  // GUID identificador unico y global. hace una nueva instancia de guid
+                ActualImageRoom = ImageUniqueName + Path.GetExtension(objRoomViewModel.Image.FileName);
+                objRoomViewModel.Image.SaveAs(Server.MapPath("~/RoomImagens/" + ActualImageRoom));
+
+
+
+                Room objRoom = new Room()
+                {
+                    RoomCapacity = objRoomViewModel.RoomCapacity,
+                    RoomDescription = objRoomViewModel.RoomDescription,
+                    RoomPrice = objRoomViewModel.RoomPrice,
+                    BookingStatusId = objRoomViewModel.BookingStatusId,
+                    RoomImage = ActualImageRoom,
+                    IsActive = true,
+                    RoomNumber = objRoomViewModel.RoomNumber,
+                    RoomTypeId = objRoomViewModel.RoomTypeId,
+
+
+                };
+                db.Rooms.Add(objRoom);
+                message = "Added.";
+            }
+            else
+            {
+                Room objRoom = db.Rooms.Single(model => model.RoomId == objRoomViewModel.RoomId);
+                if (objRoomViewModel.Image != null)
+                {
+                    ImageUniqueName = Guid.NewGuid().ToString();  // GUID identificador unico y global. hace una nueva instancia de guid
+                    ActualImageRoom = ImageUniqueName + Path.GetExtension(objRoomViewModel.Image.FileName);
+                    objRoomViewModel.Image.SaveAs(Server.MapPath("~/RoomImagens/" + ActualImageRoom));
+                    objRoom.RoomImage = ActualImageRoom;
+                }
+                objRoom.RoomCapacity = objRoomViewModel.RoomCapacity;
+                objRoom.RoomDescription = objRoomViewModel.RoomDescription;
+                objRoom.RoomPrice = objRoomViewModel.RoomPrice;
+                objRoom.BookingStatusId = objRoomViewModel.BookingStatusId;
+                objRoom.IsActive = true;
+                objRoom.RoomNumber = objRoomViewModel.RoomNumber;
+                objRoom.RoomTypeId = objRoomViewModel.RoomTypeId;
+                message = "Update.";
+            }
+                db.SaveChanges();
+            
+
+            return Json(new { message="Reservacion "+message, success=true}, JsonRequestBehavior.AllowGet);
+        }
+        public PartialViewResult GetAllRooms()
+        {
+            IEnumerable<RoomDetailsViewModel> listOfRoomDetailsViewModel = (from d in db.Rooms
+                                                                            join objBooking in db.BookingStatus on d.BookingStatusId equals objBooking.BookingStatusId
+                                                                            join objRoomType in db.RoomTypes on d.RoomTypeId equals objRoomType.RoomTypeId
+                                                                            where d.IsActive == true
+                                                                            select new RoomDetailsViewModel()
+                                                                            {
+                                                                                RoomNumber = d.RoomNumber,
+                                                                                RoomCapacity = d.RoomCapacity,
+                                                                                RoomImage = d.RoomImage,
+                                                                                RoomPrice = d.RoomPrice,
+                                                                                BookingStatus = objBooking.BookingStatus,
+                                                                                RoomType = objRoomType.RoomTypeName,
+                                                                                RoomId = d.RoomId
+
+                                                                            }).ToList();
+
+            return PartialView("_RoomDetailsPartial", listOfRoomDetailsViewModel);
+        }
+        [HttpGet]
+        public JsonResult EditRoomDetails(int RoomId)
+        {
+            var result = db.Rooms.Single(d => d.RoomId == RoomId);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult DeleteRoomDetails(int RoomId)
+        {
+            Room objRoom= db.Rooms.Single(d => d.RoomId == RoomId);
+            objRoom.IsActive = false;
+            db.SaveChanges();
+
+            return Json(new { Message="Borrado con exito", Success=true} , JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ HotelBookingWebApp/Scripts/IndexScript.js
@@ -0,0 +1,135 @@
+﻿$(document).ready(function () {
+    LoadRoomDetails();
+    $("#txtRoomId").val("0")
+    $("#btnSave").click(function () {
+        if (!$("#formRoom").valid()) {
+            return;
+        }
+        SaveRoomData();
+    });
+});
+
+function DeleteRoom(RoomId) {
+    var result = confirm("Seguro que quiero borrar reserva?");
+    if (result===false) {
+        return false;
+    }
+    $.ajax({
+        async: true,
+        type: 'GET',
+        dataType: 'JSON',
+        contentType: 'application/json; charset=utf-8',
+        data: { RoomId: RoomId },
+        url: '/Room/DeleteRoomDetails',
+        success: function (data) {
+            if (data.Success === true) {
+                alert(data.Message);
+                LoadRoomDetails();
+            }
+        },
+        error: function () {
+            alert("There is some problem to process your requets")
+        }
+    });
+}
+
+function EditRoom(RoomId) {
+    $.ajax({
+        async: true,
+        type: 'GET',
+        dataType: 'JSON',
+        contentType: 'application/json; charset=utf-8',
+        data: { RoomId:RoomId },
+        url: '/Room/EditRoomDetails',
+        success: function (data) {
+            $("#txtRoomNumber").val(data.RoomNumber);
+            $("#txtRoomPrice").val(data.RoomPrice);
+            $("#ddBookingStatus").val(data.BookingStatusId);
+            $("#ddRoomType").val(data.RoomTypeId);
+            $("#txtRoomCapacity").val(data.RoomCapacity);
+            $("#txtRoomDescription").val(data.RoomDescription);
+            $("#imagRoom").attr('src', "../RoomImagens/"+data.RoomImage);
+            $("#divAddRoom").modal({ show: true });
+            $("#txtRoomId").val(data.RoomId);
+            $("#btnSave").text("Update");
+        },
+        error: function () {
+            alert('No se pudo borrar la reserva');
+        }
+    });
+}
+
+function LoadRoomDetails() {
+    $.ajax({
+        async: true,
+        data: 'GET',
+        dataType: 'HTML',
+        contentType: false,
+        processData: false,
+        url: '/Room/GetAllRooms',
+        success: function (data) {
+            $("#idRoomLoadDetails").html(data);
+        },
+        error: function () {
+            alert("Hubo un error en el proceso de request");
+        }
+
+    })
+}
+
+function SaveRoomData() {   // funcion para salvar la seleccion de habitacion
+    var formData = new FormData(); //creo variable para crear pares de clave/valor lista para mandar esa informacion.
+    formData.append("RoomId", $("#txtRoomId").val());
+    formData.append("RoomNumber", $("#txtRoomNumber").val());  //con .append mandamos pares de clave/valor al FormData()
+    formData.append("RoomPrice", $("#txtRoomPrice").val());
+    formData.append("BookingStatusId", $("#ddBookingStatus").val());
+    formData.append("RoomTypeId", $("#ddRoomType").val());
+    formData.append("RoomCapacity", $("#txtRoomCapacity").val());
+    formData.append("RoomDescription", $("#txtRoomDescription").val());
+    formData.append("Image", $("#UpLoadImg").get(0).files[0]);
+
+
+    $.ajax({
+        async: true,
+        type: 'POST',
+        dataType: 'JSON',
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function (data) {
+            if (data.success===true) {
+                alert(data.message)
+                ResertItem()
+                
+                LoadRoomDetails()
+            }
+        },
+        error: function () {
+            alert("No se pudo hacer la reserva");
+        }
+
+    })
+}
+function ResertItem() {
+    $("#txtRoomNumber").val(''); 
+    $("#txtRoomPrice").val('');
+    $("#ddBookingStatus").val(1);
+    $("#ddRoomType").val(1);
+    $("#txtRoomCapacity").val('');
+    $("#txtRoomDescription").val('');
+    $("#UpLoadImg").focus();
+    $("#imagRoom").removeAttr('src');
+    $("#txtRoomId").val(0);
+    $("#btnSave").val("save");
+
+}
+
+function DisplayImage(result) {
+    if (result.files && result.files[0]) {
+        var fileReader = new FileReader(); //permite a la app leer ficheros
+        fileReader.onload = function (e) {
+            $("#imagRoom").attr('src', e.target.result);  //.target.   Una referencia a un objeto que lanzo el evento
+        }
+        fileReader.readAsDataURL(result.files[0]);  //El método readAsDataURL es usado para leer el contenido del especificado File.
+    }
+}
